f53930fe2787858aace03b99c46bdd5f727d4fcd
Said Chakir <saidchakir1020@gmail.com>
2025-11-13 11:27:42 +0200

    feat: added exercise7 javascript file

--- Javascript/Module1/Ex7/ex7.js
+++ Javascript/Module1/Ex7/ex7.js
@@ -1,1 +1,16 @@
+let rolls = parseInt(prompt("How many dice do you want to roll?"));
 
+if (isNaN(rolls) || rolls <= 0) {
+  document.write("Please enter a valid positive number of rolls.");
+} else {
+  let sum = 0;
+
+
+  for (let i = 0; i < rolls; i++) {
+
+    let roll = Math.floor(Math.random() * 6) + 1;
+    sum += roll;
+  }
+
+  document.write(`You rolled ${rolls} dice. The sum of the results is ${sum}.`);
+}
