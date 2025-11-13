ed48043a36c6fdcd6c0f4a76129e33cc60a67c60
Said Chakir <saidchakir1020@gmail.com>
2025-11-13 11:18:48 +0200

    feat: added exercise5 javascript file

--- Javascript/Module1/Ex5/ex5.js
+++ Javascript/Module1/Ex5/ex5.js
@@ -1,1 +1,11 @@
+'use strict'
 
+const year = prompt('Enter a year:')
+
+if (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0)
+{document.write(`${year} is a leap year`)}
+
+else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)
+{document.write(`${year} is a leap year`)}
+
+else {document.write(`${year} is not a leap year`)}
