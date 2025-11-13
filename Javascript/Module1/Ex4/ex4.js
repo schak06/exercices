0e9d571064f510dde6c2f21736f1c84283473df1
Said Chakir <saidchakir1020@gmail.com>
2025-11-13 11:14:21 +0200

    feat: added exercise4 javascript file

--- Javascript/Module1/Ex4/ex4.js
+++ Javascript/Module1/Ex4/ex4.js
@@ -1,1 +1,15 @@
+let name = prompt("Enter the student's name:");
 
+let draw = Math.floor(Math.random() * 4) + 1;
+
+let house;
+if (draw === 1) {
+  house = "Gryffindor";
+} else if (draw === 2) {
+  house = "Slytherin";
+} else if (draw === 3) {
+  house = "Hufflepuff";
+} else {
+  house = "Ravenclaw";
+}
+document.write(`${name}, you are ${house}.`);
