a1fe6dfc7ff584ccfd814ddab51b4ffc704c9622
Said Chakir <saidchakir1020@gmail.com>
2025-11-13 11:23:56 +0200

    feat: added exercise6 javascript file

--- Javascript/Module1/Ex6/ex6.js
+++ Javascript/Module1/Ex6/ex6.js
@@ -1,1 +1,10 @@
-
+'use strict'
+const sqrt = confirm('Do you want to calculate the square root?')
+if (sqrt){
+    const number = prompt('Write your number')
+if (number < 0){
+    document.write('The square root of a negative number is not defined')}
+else {
+    document.write(`The square root of ${number} is ${Math.sqrt(number)}`)}}
+else {
+    document.write('The square root is not calculated.')}
