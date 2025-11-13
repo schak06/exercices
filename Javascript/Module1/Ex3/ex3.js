d227bd364a5ae19653e97cb2027854b9013c33cc
Said Chakir <saidchakir1020@gmail.com>
2025-11-13 11:01:20 +0200

    feat: added exercise3 javascript file

--- Javascript/Module1/Ex3/ex3.js
+++ Javascript/Module1/Ex3/ex3.js
@@ -1,1 +1,10 @@
-
+'use strict';
+const num1 = Number(prompt("Enter the first number:"))
+const num2 = Number(prompt("Enter the second number:"))
+const num3 = Number(prompt("Enter the third number:"))
+const sum = num1 + num2 + num3
+const product = num1 * num2 * num3
+const average = sum / 3
+document.write(`The sum of ${num1}, ${num2}, and ${num3} is ${sum}.<br>`)
+document.write(`The product of ${num1}, ${num2}, and ${num3} is ${product}.<br>`)
+document.write(`The average of ${num1}, ${num2}, and ${num3} is ${average}.`)
