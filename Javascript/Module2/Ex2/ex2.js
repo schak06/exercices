54ab78c7fc572af539f0fcddbb166cba0ed66e0f
Said Chakir <saidchakir1020@gmail.com>
2025-11-17 09:14:10 +0200

    feat: added exercise2 javascript file

--- Javascript/Module2/Ex2/ex2.js
+++ Javascript/Module2/Ex2/ex2.js
@@ -1,1 +1,22 @@
+'use strict'
+
+let count = Number(prompt("How many participants?"));
+let participants = [];
+
+for (let i = 0; i < count; i++) {
+  let name = prompt(`Enter name of participant ${i + 1}:`);
+  participants.push(name);
+}
+
+participants.sort();
+
+let ol = document.createElement("ol");
+
+for (let i = 0; i < participants.length; i++) {
+  let li = document.createElement("li");
+  li.textContent = participants[i];
+  ol.appendChild(li);
+}
+
+document.body.appendChild(ol);
 
