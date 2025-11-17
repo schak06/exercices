afed0b5261b88eac849293b607c5b50e10de2fb8
Said Chakir <saidchakir1020@gmail.com>
2025-11-17 09:46:58 +0200

    feat: added exercise6 javascript file

--- Javascript/Module2/Ex6/ex6.js
+++ Javascript/Module2/Ex6/ex6.js
@@ -1,1 +1,17 @@
+'use strict'
+
+function diceRolls() {
+    let numOutput = Math.floor(Math.random()*7)
+
+    while (numOutput < 6) {
+
+        numOutput = Math.floor(Math.random()*7)
+
+
+        document.querySelector('#results').innerHTML += `<li>${numOutput}</li>`;
+    }
+}
+
+diceRolls()
+
 
