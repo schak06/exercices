93de34b84d6828ca6159526f6f35a73313933ced
Said Chakir <saidchakir1020@gmail.com>
2025-11-17 09:18:57 +0200

    feat: added exercise3 javascript file

--- Javascript/Module2/Ex3/ex3.js
+++ Javascript/Module2/Ex3/ex3.js
@@ -1,1 +1,21 @@
+'use strict'
 
+let dogs = [];
+
+for (let i = 0; i < 6; i++) {
+  let dog = prompt(`Enter dog name ${i + 1}:`);
+  dogs.push(dog);
+}
+
+dogs.sort();
+dogs.reverse();
+
+let ul = document.createElement("ul");
+
+for (let i = 0; i < dogs.length; i++) {
+  let li = document.createElement("li");
+  li.textContent = dogs[i];
+  ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
