16fa687a5cf055788c5e54f6335958a625ada90a
Said Chakir <saidchakir1020@gmail.com>
2025-11-17 09:06:26 +0200

    feat: added exercise1 javascript file

--- Javascript/Module2/Ex1/ex1.js
+++ Javascript/Module2/Ex1/ex1.js
@@ -1,1 +1,14 @@
+'use strict'
+let nums = [];
+
+for (let i = 0; i < 5; i++) {
+  let n = Number(prompt(`Enter number ${i + 1}:`));
+  nums.push(n);
+}
+
+console.log("Numbers in reverse order:");
+
+for (let i = nums.length - 1; i >= 0; i--) {
+  console.log(nums[i]);
+}
 
