f628724d45260394ab2c423ffb010e6a97e43f44
Said Chakir <saidchakir1020@gmail.com>
2025-11-17 09:23:45 +0200

    feat: added exercise4 javascript file

--- Javascript/Module2/Ex4/ex4.js
+++ Javascript/Module2/Ex4/ex4.js
@@ -1,1 +1,14 @@
+'use strict'
 
+let numbers = [];
+let input = Number(prompt("Enter a number (0 to stop):"));
+
+while (input !== 0) {
+  numbers.push(input);
+  input = Number(prompt("Enter a number (0 to stop):"));
+}
+
+numbers.sort((a, b) => b - a);
+
+console.log("Numbers from largest to smallest:");
+console.log(numbers);
