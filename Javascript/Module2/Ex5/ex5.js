28e0c6c52f37d605eda0a2c3027bd78f19734512
Said Chakir <saidchakir1020@gmail.com>
2025-11-17 09:29:09 +0200

    feat: added exercise5 javascript file

--- Javascript/Module2/Ex5/ex5.js
+++ Javascript/Module2/Ex5/ex5.js
@@ -1,1 +1,18 @@
+'use strict'
 
+let nums = [];
+let input = Number(prompt("Enter a number:"));
+
+while (!nums.includes(input)) {
+  nums.push(input);
+  input = Number(prompt("Enter a number:"));
+}
+
+console.log("Number has already been entered. Program stops.");
+
+nums.sort((a, b) => a - b);
+
+console.log("Numbers in ascending order:");
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums[i]);
+}
